d7a2e556b4be49feb392df70fcbd31cbbfed58c3
brianmujjuni <brianmujjuni@engineer.com>
2025-03-04 01:50:43 +0300

    create regex function to validate email

--- frontend/expense-tracker/src/pages/Auth/Login.jsx
+++ frontend/expense-tracker/src/pages/Auth/Login.jsx
@@ -1,43 +1,65 @@
-import React from 'react'
-import AuthLayout from '../../components/layouts/AuthLayout'
-import { useNavigate } from 'react-router-dom'
-import Input from '../../components/inputs/input'
+import React from "react";
+import AuthLayout from "../../components/layouts/AuthLayout";
+import { Link, useNavigate } from "react-router-dom";
+import Input from "../../components/inputs/input";
 export default function Login() {
-  const [email, setEmail] = React.useState('')
-  const [password, setPassword] = React.useState('')
-  const [loading, setLoading] = React.useState(false)
-  const [error, setError] = React.useState('')
-  const navigate = useNavigate()
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [loading, setLoading] = React.useState(false);
+  const [error, setError] = React.useState("");
+  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
-
-  }
+    e.preventDefault();
+    setLoading(true);
+    if(!validateEmail(email)){
+      setError("Please enter a valid email address");
+      setLoading(false);
+      return;
+    }
+    if(password.length >= 8){
+      setError("Password must be at least 8 characters");
+      setLoading(false);
+      return;
+    }
+    setError("");
+    const data = { email, password };
+  };
   return (
     <AuthLayout>
-    <div className='lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center '>
-      <h3 className='text-xl font-semibold text-black'>Welcome Back</h3>
-      <p className='text-xs text-slate-700 mt-[5px] mb-6'>Please enter your details to log in</p>
-      <form onSubmit={()=>{}}>
-        <Input
-          value={email}
-          onchange={(e) => setEmail(e.target.value)}
-          placeholder='Enter your email'
-          type='email'
-          label='Email'
-        />
-        <Input
-          value={password}
-          onchange={(e) => setPassword(e.target.value)}
-          placeholder='Enter your password'
-          type='password'
-          label='Password'
-        />
-        <button type='submit' className='btn-primary mt-6' disabled={loading}>
-          {loading ? 'Loading...' : 'Login'}
-        </button>
-        {error && <p className='text-red-500 mt-4'>{error}</p>}
-      </form>
-    </div>
+      <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center ">
+        <h3 className="text-xl font-semibold text-black">Welcome Back</h3>
+        <p className="text-xs text-slate-700 mt-[5px] mb-6">
+          Please enter your details to log in
+        </p>
+        <form onSubmit={() => {}}>
+          <Input
+            value={email}
+            onchange={(e) => setEmail(e.target.value)}
+            placeholder="Enter your email"
+            type="email"
+            label="Email"
+          />
+          <Input
+            value={password}
+            onchange={(e) => setPassword(e.target.value)}
+            type="password"
+            label="Password"
+            placeholder="Min 8 Characters"
+          />
+          <button type="submit" className="btn-primary mt-6" disabled={loading}>
+            {loading ? "Loading..." : "Login"}
+          </button>
+          {error && <p className="text-red-500 text-xs pb-2.5 ">{error}</p>}
+          
+          <p className="text-[13px] text-slate-800 mt-3">
+            Don't have an account?{" "}
+            <Link className="font-medium text-primary underline" to="/signup">
+              SignUp
+            </Link>
+          </p>
+        </form>
+      </div>
     </AuthLayout>
-  )
+  ); 
 }
