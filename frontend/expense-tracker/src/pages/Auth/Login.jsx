00a6474290093151952973119d1b5f5b607570bc
brianmujjuni <brianmujjuni@engineer.com>
2025-03-03 23:42:07 +0300

    create auth main layout

--- frontend/expense-tracker/src/pages/Auth/Login.jsx
+++ frontend/expense-tracker/src/pages/Auth/Login.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import AuthLayout from '../../components/layouts/AuthLayout'
 
 export default function Login() {
   return (
+    <AuthLayout>
     <div className='lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center '>
       <h3 className='text-xl font-semibold text-black'>Welcome Back</h3>
       <p className='text-xs text-slate-700 mt-[5px] mb-6'>Please enter your details to log in</p>
     </div>
+    </AuthLayout>
   )
 }
