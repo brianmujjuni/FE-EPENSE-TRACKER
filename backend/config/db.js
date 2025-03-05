6aa1436869e051c7881601f27580239e40518c1b
brianmujjuni <brianmujjuni@engineer.com>
2025-03-05 16:29:10 +0300

    add user register action & db connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const connectDb = async () => {
     try {
-        console.log("Connecting to MongoDB...");
+        
+        mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
     } catch (error) {
         console.error("Error connecting to MongoDB", error);
         process.exit(1);
--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,13 +1,40 @@
-const jwt = require('jsonwebtoken');
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
 
 const genearateToken = (id) => {
-    return jwt.sign({id},process.env.JWT_SECRET,{
-        expiresIn:"1h"
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "1h",
+  });
+};
+
+exports.registerUser = async (req, res) => {
+  const { fullName, email, password, profileImageUrl } = req.body;
+  if (!fullName || !email || !password) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+  try {
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+    const user = await User.create({
+      fullName,
+      email,
+      password,
+      profileImageUrl,
     });
-}
+    return res.status(201).json({
+      id: user._id,
+      user,
+      token: genearateToken(user._id),
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error registering user", error: error.message });
+  }
+};
 
-exports.registerUser = async (req,res) => {}
+exports.loginUser = async (req, res) => {};
 
-exports.loginUser = async (req,res) => {}
-
-exports.getUserInfo = async (req,res) => {}
+exports.getUserInfo = async (req, res) => {};
