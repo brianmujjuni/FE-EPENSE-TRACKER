c014d5c27ed4ef43e3285353f444435de972581f
brianmujjuni <brianmujjuni@engineer.com>
2025-03-06 00:46:35 +0300

    create income schema

---
+++ backend/controllers/incomeController.js
@@ -0,0 +1,7 @@
+const User = require("../models/userModel");
+const Income = require("../models/incomeModel");
+exports.addIncome = async (req, res) => {};
+
+exports.getAllIncome = async (req, res) => {};
+exports.deleteIncome = async (req, res) => {};
+exports.downloadIncomeExcel = async (req, res) => {};
