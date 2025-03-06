de2eb94c23a5bb116c617742748179aa3c41e3db
brianmujjuni <brianmujjuni@engineer.com>
2025-03-07 00:09:05 +0300

    create expense routes

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const path = require("path");
 const connectDb = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 const incomeRoutes = require("./routes/incomeRoutes");
+const expenseRoutes = require("./routes/expenseRoutes")
 const app = express();
 
 app.use(
@@ -21,6 +22,7 @@ connectDb();
 
 app.use("/api/v1/auth",authRoutes);
 app.use("/api/v1/income",incomeRoutes);
+app.use("/api/v1/expense",expenseRoutes)
 
 app.use("/uploads",express.static(path.join(__dirname,'uploads')))
 
