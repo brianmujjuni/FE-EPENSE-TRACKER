2da185e91ee9f0d27870ebdf5ae2983a89f55b51
brianmujjuni <brianmujjuni@engineer.com>
2025-03-07 01:22:05 +0300

    create expens actions in the controller

--- backend/controllers/expenseController.js
+++ backend/controllers/expenseController.js
@@ -1,18 +1,61 @@
-const xlsx = require("xlsx")
-const Expense = require("../models/Expense")
+const xlsx = require("xlsx");
+const Expense = require("../models/Expense");
 
-exports.addExpense = async(req,res)=>{
+exports.addExpense = async (req, res) => {
+  const userId = req.user.id;
+  try {
+    const { icon, category, amount, date } = req.body;
+    if (!category || !amount || !date) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+    const expense = Expense.create({
+      userId,
+      icon,
+      category,
+      amount,
+      date,
+    });
+    await expense.save();
+    res.status(201).json(expense);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
-}
+exports.getAllExpense = async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const expenses = Expense.find({ userId }).sort({ date: -1 });
+    res.status(200).json(expenses);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
-exports.getAllExpense = async(req,res)=>{
+exports.deleteExpense = async (req, res) => {
+  try {
+    await Expense.findByIdAndDelete(req.params.id);
+    res.status(200).json({ message: "Expense Successfully deleted" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
-}
-
-exports.deleteExpense = async(req,res)=>{
-
-}
-
-exports.downloadExpenseExcel = async(req,res)=>{
-    
-}
+exports.downloadExpenseExcel = async (req, res) => {
+  const userId = req.user.id;
+  try {
+    const expenses = await Expense.find({ userId }).sort({ date: -1 });
+    const data = expenses.map((item) => ({
+      Category: item.category,
+      Amount: item.amount,
+      Date: item.date,
+    }));
+    const wb = xlsx.utils.book_new()
+    const ws = xlsx.utils.json_to_sheet(data)
+    xlsx.utils.book_append_sheet(wb,ws,"Expenses")
+    xlsx.writeFile(wb,"expenses_details.xlsx")
+    res.download("expenses_details.xlsx")
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
