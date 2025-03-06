ea02105cb8fe6ec203759b45bdc0f5cd74660e6d
brianmujjuni <brianmujjuni@engineer.com>
2025-03-07 00:35:18 +0300

    create expense schema

---
+++ backend/controllers/expenseController.js
@@ -0,0 +1,18 @@
+const xlsx = require("xlsx")
+const Expense = require("../models/Expense")
+
+exports.addExpense = async(req,res)=>{
+
+}
+
+exports.getAllExpense = async(req,res)=>{
+
+}
+
+exports.deleteExpense = async(req,res)=>{
+
+}
+
+exports.downloadExpenseExcel = async(req,res)=>{
+    
+}
